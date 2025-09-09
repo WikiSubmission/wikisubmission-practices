298ed943df4e2b5b384cf56f76da29ac4b26bb7b
Sarim <sarimyx@proton.me>
2025-09-09 09:45:46 -0600

    old files

--- src/types/w-file-result.ts
+++
@@ -1,9 +0,0 @@
-export type WFileResult = {
-    name: string;
-    folder: string;
-    path: string;
-    type: string;
-    score: number;
-    url: string;
-    urls: { provider: string; url: string }[];
-};
