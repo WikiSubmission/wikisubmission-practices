3e3e6eabde7da6a5d12196ea8451e01d183c6e1b
Sarim <sarimyx@proton.me>
2025-10-07 21:44:19 -0600

    disable route level cache

--- src/routes/prayer-times.ts
+++ src/routes/prayer-times.ts
@@ -9,10 +9,6 @@ export default function route(): WRoute {
     return {
         url: "/prayer-times/:q?",
         method: "GET",
-        cache: {
-            duration: 45,
-            durationType: "seconds"
-        },
         handler: async (request, reply) => {
             try {
                 // Extract location from URL path (after /prayer-times/) or from query parameters
